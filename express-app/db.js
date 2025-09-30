efcd38174db550c4d0b3ad29e2157875cfb91701
Wan11703 <vincentmark111703@gmail.com>
2025-09-30 13:18:16 +0800

    Changed host

--- express-app/db.js
+++ express-app/db.js
@@ -10,7 +10,7 @@ const dbConnection = mysql.createConnection({
   database: process.env.DB_NAME,
   connectionLimit: 10
 });
-// Changed port
+// Changed host
 // test the connection
 dbConnection.connect((error) => {
   if (error) {
