8b46a1217e1ecee88174ef3956487670a43fa831
Wan11703 <vincentmark111703@gmail.com>
2025-09-30 12:20:44 +0800

    Changed url and port

--- express-app/db.js
+++ express-app/db.js
@@ -10,7 +10,7 @@ const dbConnection = mysql.createPool({
   database: process.env.DB_NAME,
   connectionLimit: 10
 });
-// Changed the url -
+// Changed url
 // test the connection
 dbConnection.getConnection((err, connection) => {
   if (err) {
