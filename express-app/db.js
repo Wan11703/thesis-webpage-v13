9da88210f964f1e5d568232058848989c4f032d5
Wan11703 <vincentmark111703@gmail.com>
2025-09-30 12:49:43 +0800

    Changed from createPool to createConn

--- express-app/db.js
+++ express-app/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 require("dotenv").config();
 var colors = require('colors');
 
-const dbConnection = mysql.createPool({
+const dbConnection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
